d96933553ee9f53e2aa038bb8352dacf9525af1a
Raaynbo <rayan.boudjema24@gmail.com>
2024-07-29 15:27:14 +0200

    book can be removed of library by using the delete button

--- asset/main.js
+++ asset/main.js
@@ -5,21 +5,21 @@ const mainzone = document.querySelector("#main");
 function Book(name, author, desc){
 	this.name = name;
 	this.author= author;
-	this.desc = desc ;
+	this.desc = desc;
+	this.id = index;
+	index++;
 }
 
 const library = []
 
+let index = 0;
 
 function addToLibrary(book){
 	library.push(book);
 	reloadDisplay();
 }
 
-function reloadDisplay(){
-	console.log("reload page should happen now");
-	// we iterate over library and create a card for each
-	library.forEach(book => {
+function createCard(book){
 		const card = document.createElement("div");
 		card.className = "card";
 		const card_title = document.createElement("span")
@@ -31,17 +31,40 @@ function reloadDisplay(){
 		card_action_container.className = "card_action_container";
 		const card_action1 = document.createElement("span");
 		card_action1.textContent = "Delete";
+		card.setAttribute("id", book.id);
 		card_action1.className = "card_action";
 		card_action_container.appendChild(card_action1);
+		
+	card_action1.addEventListener("click", (e) => {
 
+		const removedid = e.target.parentNode.parentNode.id;
+		e.target.parentNode.parentNode.remove();
+		let elementToDelete = library.filter((book) => book.id == removedid)
+		let indexOfDeletel = library.indexOf(elementToDelete[0]) 
+		library.splice(indexOfDeletel,1);
+		console.log(library);
+	});
 		card.appendChild(card_title);
 		card.appendChild(card_content);
 		card.appendChild(card_action_container);
 
 		mainzone.appendChild(card);
-	});
+}
+
+function reloadDisplay(){
+	while (mainzone.firstChild){
+		mainzone.removeChild(mainzone.firstChild);	
+	}
+	// we iterate over library and create a card for each
+	library.forEach(book => createCard(book)
+	);
+}
+
+function openModal(){
+	const modal = document.querySelector("dialog");
+	modal.showModal();
+
 }
 
 const book1 = new Book("LOTR", "Tolkien", "Men discussing about a ring");
 addToLibrary(book1);
-console.log(book1);
--- log.md
+++ log.md
@@ -25,3 +25,13 @@ side main main main main
 
 made a function creating a placeholder book card
 I want to make a modal form for book creation or I need to find a way to incorporate the add book form
+
+# 2024-07-29 - 3.20pm
+
+I thought about it yesterday before going to bed, I need to add an id to each book while creating them in order to facilitate the removing action.
+
+My way to do it is: get back the id by checking parentNode id.
+use the found id to identify position of the book inside the library array
+then we splice the library array using the info and remove the card from the DOM
+
+(it works, I just don't like this line to remove the DOM element 		`e.target.parentNode.parentNode.remove();`) 
